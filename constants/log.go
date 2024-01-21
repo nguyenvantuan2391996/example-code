5709e8799f63f7bb37f55334c59166173b88e930
tuannguyen <nguyenvantuan2391996@gmail.com>
2024-01-21 10:19:54 +0700

    feat: add some constants

---
+++ constants/log.go
@@ -0,0 +1,11 @@
+package constants
+
+const (
+	BeginningTaskMessage        = "beginning task = %s info input = %v"
+	TaskErrorMessage            = "task = %s is failed with err = %v"
+	TaskErrorWithInputMessage   = "task = %s is failed with input = %v and err = %v"
+	GetEntityErrorMessage       = "get entity = %s info input = %v fail with err = %v"
+	UpdateEntityErrorMessage    = "update entity = %s info input = %v fail with err = %v"
+	CreateEntityErrorMessage    = "create entity = %s info input = %v fail with err = %v"
+	GetEntityClientErrorMessage = "get entity client = %s info input = %v fail with err = %v"
+)
