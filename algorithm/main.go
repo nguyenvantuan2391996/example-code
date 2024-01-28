234b5e3573f2fe2658ddae1d8e6ad5475407c396
tuannguyen <nguyenvantuan2391996@gmail.com>
2024-01-28 23:01:02 +0700

    feat: add quick sort

--- algorithm/main.go
+++ algorithm/main.go
@@ -1,10 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"example-code/algorithm/sort"
+)
 
 func main() {
 	nums := []int{3, 2, 5, 0, 1, 8, 7, 6, 9, 4}
 	fmt.Println(fmt.Sprintf("Before: %v", nums))
-	QuickSort(nums)
+	sort.QuickSort(nums)
 	fmt.Println(fmt.Sprintf("After: %v", nums))
 }
