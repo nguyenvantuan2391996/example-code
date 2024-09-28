bddb2e384fd309667107983341c963a5ac4453f8
tuannguyen <nguyenvantuan2391996@gmail.com>
2024-09-29 00:11:35 +0700

    feat: implement google docs using websocket

--- google-docs-using-websocket/main.go
+++ google-docs-using-websocket/main.go
@@ -14,7 +14,7 @@ var upgrader = websocket.Upgrader{
 	WriteBufferSize: 1024,
 }
 
-var mapWsConn = make(map[string]*websocket.Conn)
+var mapWsConn = make(map[string]map[string]*websocket.Conn)
 
 func main() {
 	http.HandleFunc("/index", LoadPage)
@@ -39,46 +39,59 @@ func LoadPage(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Fprintf(w, "%s", content)
+	_, err = fmt.Fprintf(w, "%s", content)
+	if err != nil {
+		return
+	}
 }
 
 func InitWebsocket(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 
 	channel := r.URL.Query().Get("channel")
+	uuid := r.URL.Query().Get("uuid")
 	if r.Header.Get("Origin") != "http://"+r.Host {
 		http.Error(w, "the origin is invalid", http.StatusInternalServerError)
 		return
 	}
 
-	if _, ok := mapWsConn[channel]; !ok {
-		conn, err := upgrader.Upgrade(w, r, nil)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		mapWsConn[channel] = conn
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
+
+	if len(mapWsConn[channel]) == 0 {
+		mapWsConn[channel] = make(map[string]*websocket.Conn)
+	}
+
+	mapWsConn[channel][uuid] = conn
 }
 
 func SaveData(w http.ResponseWriter, r *http.Request) {
 	channel := r.FormValue("channel")
+	uuid := r.FormValue("uuid")
 	data := r.FormValue("data")
 
 	if _, ok := mapWsConn[channel]; !ok {
 		http.Error(w, "the channel is not found", http.StatusInternalServerError)
 	}
 
-	err := mapWsConn[channel].WriteJSON(map[string]interface{}{
-		"data": data,
-	})
-	if err != nil {
-		return
+	for key, ws := range mapWsConn[channel] {
+		if key != uuid {
+			err := ws.WriteJSON(map[string]interface{}{
+				"data": data,
+			})
+			fmt.Println(websocket.IsCloseError(err))
+			fmt.Println(websocket.IsUnexpectedCloseError(err))
+			if err != nil {
+				return
+			}
+		}
 	}
 
 	w.WriteHeader(http.StatusOK)
-	_, err = w.Write([]byte("success"))
+	_, err := w.Write([]byte("success"))
 	if err != nil {
 		return
 	}
