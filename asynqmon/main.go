83c8c2e6b683efb69995cfa6d23847aef395edd0
Tuan Nguyen Van <38428343+nguyenvantuan2391996@users.noreply.github.com>
2025-05-16 10:04:40 +0700

    feat: add asynq

---
+++ asynqmon/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/hibiken/asynq"
+)
+
+func main() {
+	client := asynq.NewClient(asynq.RedisClientOpt{Addr: "0.0.0.0:6379", Password: ""})
+	defer func(client *asynq.Client) {
+		err := client.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(client)
+
+	payload := []byte("Hello World")
+
+	info, err := client.Enqueue(asynq.NewTask("hihi", payload), asynq.ProcessIn(10*time.Second))
+	if err != nil {
+		log.Fatalf("could not schedule task: %v", err)
+	}
+	log.Printf("enqueued task: id=%s queue=%s", info.ID, info.Queue)
+
+	go func() {
+		srv := asynq.NewServer(
+			asynq.RedisClientOpt{Addr: "0.0.0.0:6379", Password: ""},
+			asynq.Config{
+				// Specify how many concurrent workers to use
+				Concurrency: 10,
+				// Optionally specify multiple queues with different priority.
+				Queues: map[string]int{
+					"critical": 6,
+					"default":  3,
+					"low":      1,
+				},
+				// See the godoc for other configuration options
+				TaskCheckInterval: 5 * time.Second,
+			},
+		)
+
+		mux := asynq.NewServeMux()
+		mux.HandleFunc("hihi", func(ctx context.Context, task *asynq.Task) error {
+			fmt.Println(fmt.Sprintf("hihi: %s", task.Payload()))
+			return nil
+		})
+
+		if err := srv.Run(mux); err != nil {
+			log.Fatalf("could not run server: %v", err)
+		}
+	}()
+
+	time.Sleep(time.Hour)
+}
