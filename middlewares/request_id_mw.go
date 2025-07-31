b3d436ea59882ea52692c36bcc0408c310a6c9e4
Tuan Nguyen Van <38428343+nguyenvantuan2391996@users.noreply.github.com>
2025-07-31 09:26:17 +0700

    feat: request id mw

---
+++ middlewares/request_id_mw.go
@@ -0,0 +1,15 @@
+package middlewares
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+)
+
+func RequestIDMW() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		requestID := uuid.New().String()
+		ctx.Request.Header.Set("X-Request-ID", requestID)
+		ctx.Set(constants.RequestIDField, requestID)
+		ctx.Next()
+	}
+}
