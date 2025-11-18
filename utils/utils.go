be56246f11c4f31b196e3fe08769ad5b9c902abd
Tuan Nguyen Van <38428343+nguyenvantuan2391996@users.noreply.github.com>
2025-11-18 21:26:50 +0700

    update: validate email

--- utils/utils.go
+++ utils/utils.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"fmt"
+	"regexp"
 )
 
 func MakeLockKey(category, identifier string) string {
@@ -10,4 +11,14 @@ func MakeLockKey(category, identifier string) string {
 
 func SpanName(action, entity, scope string) string {
 	return fmt.Sprintf("%s_%s_%s", action, entity, scope)
+}
+
+func IsValidEmail(email string) bool {
+	const emailRegex = `^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$`
+	
+	if len(email) < 3 || len(email) > 254 {
+		return false
+	}
+	match, _ := regexp.MatchString(emailRegex, email)
+	return match
 }
