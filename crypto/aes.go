d30ceeb01c5da5acb585dc5734fef5de3ef05633
Tuan Nguyen Van <38428343+nguyenvantuan2391996@users.noreply.github.com>
2025-11-18 16:57:42 +0700

    feat: aes

---
+++ crypto/aes.go
@@ -0,0 +1,44 @@
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"io"
+)
+
+func AesEncrypt(plaintext, key []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
+		return nil, err
+	}
+
+	return gcm.Seal(nonce, nonce, plaintext, nil), nil
+}
+
+func AesDecrypt(ciphertext, key []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+
+	nonceSize := gcm.NonceSize()
+
+	nonce := ciphertext[:nonceSize]
+	data := ciphertext[nonceSize:]
+
+	return gcm.Open(nil, nonce, data, nil)
+}
