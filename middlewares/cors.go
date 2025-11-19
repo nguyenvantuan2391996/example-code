171c295dde0d8dfbd287c46cb4424154bc9a4f3e
Tuan Nguyen Van <38428343+nguyenvantuan2391996@users.noreply.github.com>
2025-11-19 09:57:48 +0700

    feat: cors mw

---
+++ middlewares/cors.go
@@ -0,0 +1,18 @@
+package middlewares
+
+import "github.com/gin-gonic/gin"
+
+func CORS() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Authorization, Content-Type")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
